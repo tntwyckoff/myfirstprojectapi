b794330a2722d0bc97ea65205f2413a6d2ceece4
Tom W <tom@dahtkahm.com>
2022-03-24 23:00:25 -0400

    corrected api listening port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response, Application} from 'express';
 
 const app:Application = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 
 app.get("/", (req:Request, res:Response) => {
