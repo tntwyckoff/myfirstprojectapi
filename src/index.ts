9cbe3360524f8123221b027e36dda56018956fa9
Tom W <tom@dahtkahm.com>
2022-03-28 22:48:18 -0400

    added quick, sirty logging middleware

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ const app:Application = express();
 const PORT = process.env.PORT || 8080;
 
 
+const loggingMiddleware = function (req:Request, res:Response, next:any) {
+    console.log(`${new Date()} REQ: ${req.url}`); 
+    next();    
+}
+
+app.use(loggingMiddleware);
+  
 app.get("/", (req:Request, res:Response) => {
-    console.log(`REQ: <root>`); 
-    res.send("Hello Typescript with Node.js!");
+    // console.log(`REQ: <root>`); 
+    res.send("Hello Typescript with Node.js! (Now with middleware!)");
 });
 
 app.get("*", function (req, res) {
-    console.log(`REQ: ${req.url}`); 
+    // console.log(`REQ: ${req.url}`); 
     res.send(`You requested: ${req.url}`);
 });
 
